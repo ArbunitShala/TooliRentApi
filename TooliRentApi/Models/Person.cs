ee1c557463893117c47d63777b6b5cf69a92b62b
ArbunitShala <arbunitshalaa@gmail.com>
2025-09-03 14:44:51 +0200

    ..

---
+++ TooliRentApi/Models/Person.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TooliRentApi.Models
+{
+    public class Person
+    {
+            public int Id { get; set; }
+            public string FirstName { get; set; } = default!;
+            public string LastName { get; set; } = default!;
+            public string Email { get; set; } = default!;
+        }
+}
